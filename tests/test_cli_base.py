20103f2fbc2f3ec22a85ce0817f9ae75fc34471c
Dan LaManna <dan.lamanna@kitware.com>
2022-02-27 12:55:47 -0500

    Add version test

--- tests/test_cli_base.py
+++ tests/test_cli_base.py
@@ -1,3 +1,7 @@
+from packaging.version import Version
+import pytest
+
+
 def test_base_command(cli_run):
     result = cli_run()
 
@@ -8,3 +12,23 @@ def test_base_command(cli_run):
     help_result = cli_run(['--help'])
     assert help_result.exit_code == 0
     assert help_result.output == result.output
+
+
+@pytest.mark.parametrize(
+    'current_version,latest_version,expected_exit_code,output_pattern',
+    [
+        [Version('0.0.1'), Version('0.1.0'), 0, 'new version'],
+        [Version('0.0.1'), Version('1.0.0'), 1, 'new major version'],
+    ],
+)
+def test_new_version(
+    cli_run, mocker, current_version, latest_version, expected_exit_code, output_pattern
+):
+    mocker.patch('isic_cli.utils.version.get_version', return_value=current_version)
+    mocker.patch('isic_cli.utils.version.newest_version_available', return_value=latest_version)
+
+    # The command is arbitrary, it just normally exits 0 with no mocking necessary
+    result = cli_run(['auth', 'print-token'])
+
+    assert result.exit_code == expected_exit_code
+    assert output_pattern in result.output
