9e3c7e60fa3c53e3c1038f003e57366667365eab
Dan LaManna <dan.lamanna@kitware.com>
2022-02-27 11:49:14 -0500

    Improve version detection and upgrade hinting

--- isic_cli/utils/version.py
+++ isic_cli/utils/version.py
@@ -1,55 +1,69 @@
 from importlib.metadata import PackageNotFoundError, version
 import sys
+from typing import Optional
 
 import click
+from packaging.version import Version
 from pkg_resources import parse_version
 import requests
 from requests.exceptions import RequestException
 
 
-def get_version():
+def get_version() -> Optional[Version]:
     try:
-        v = version('isic-cli')
-        if v == '0.0.0':  # TODO: this only occurs in testing.
-            return None
+        return parse_version(version('isic-cli'))
     except PackageNotFoundError:
         # package is not installed
         return None
 
 
-def newer_version_available():
-    version = get_version()
-    if version is None:
-        return False
+def upgrade_type(from_version: Version, to_version: Version) -> Optional[str]:
+    if to_version.major > from_version.major:
+        return 'major'
+    elif to_version.minor > from_version.minor:
+        return 'minor'
+    elif to_version.micro > from_version.micro:
+        return 'micro'
 
-    this_version = parse_version(version)
-    if this_version.is_devrelease:
-        return False
 
+def newest_version_available() -> Optional[Version]:
     r = requests.get('https://pypi.org/pypi/isic-cli/json', timeout=(5, 5))
     r.raise_for_status()
     releases = [parse_version(v) for v in r.json()['releases'].keys()]
-    for release in releases:
-        if not (release.is_prerelease or release.is_devrelease) and release > this_version:
-            return True
-    return False
+    real_releases = [x for x in releases if not x.is_prerelease and not x.is_devrelease]
+    if real_releases:
+        return sorted(real_releases)[-1]
 
 
 def check_for_newer_version():
+    this_version = get_version()
+
+    if not this_version or this_version.is_devrelease:
+        return
+
     try:
-        if newer_version_available():
-            click.echo(
-                click.style(
-                    """There is a newer version of isic-cli available.
-You must upgrade to the latest version before continuing.
+        newest_version = newest_version_available()
+        if not newest_version:
+            return
+
+        upgrade_type_available = upgrade_type(this_version, newest_version)
+        if upgrade_type_available == 'major':
+            click.secho(
+                """There is a new major version of isic-cli available.
+You must upgrade before continuing.
 If you are using pip, then you can upgrade by running the following command:
 """,
-                    fg='yellow',
-                ),
+                fg='yellow',
                 err=True,
             )
             click.echo(click.style('pip install --upgrade isic-cli', fg='green'), err=True)
             sys.exit(1)
+        elif upgrade_type_available == 'minor':
+            click.secho(
+                "Psst, there's a new version of isic-cli available. Upgrade!\n",
+                fg='yellow',
+                err=True,
+            )
     except RequestException:
         click.echo(
             click.style('Failed to check for newer version of isic-cli:', fg='red'), err=True
