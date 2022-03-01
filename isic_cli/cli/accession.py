8c23cbd3245983889e91925ea31497f8e27af864
Dan LaManna <dan.lamanna@kitware.com>
2022-03-01 16:27:13 -0500

    Add --json flag to accession upload

--- isic_cli/cli/accession.py
+++ isic_cli/cli/accession.py
@@ -1,8 +1,10 @@
+import json
 from pathlib import Path
 import sys
 
 import click
 from requests.exceptions import HTTPError
+from rich.console import Console
 from s3_file_field_client import S3FileFieldClient
 
 from isic_cli.cli.context import IsicContext
@@ -23,32 +25,40 @@ def accession(ctx):
     'accession',
     type=click.Path(file_okay=True, dir_okay=False, exists=True, allow_dash=False, path_type=Path),
 )
+@click.option('--json', 'json_', default=False, is_flag=True, help='Format output as JSON.')
 @click.pass_obj
 @require_login
 def upload(
     ctx: IsicContext,
     cohort_id: int,
     accession: Path,
+    json_: bool,
 ):
-    from isic_cli.cli import DOMAINS
 
-    s3ff_client = S3FileFieldClient(f'{DOMAINS[ctx.env]}/api/v2/s3-upload/', ctx.session)
+    console = Console(stderr=True)
+    with console.status('Uploading'):
+        from isic_cli.cli import DOMAINS
 
-    with accession.open('rb') as file_stream:
-        field_value = s3ff_client.upload_file(
-            file_stream,
-            accession.name,
-            'ingest.Accession.original_blob',
-        )
+        s3ff_client = S3FileFieldClient(f'{DOMAINS[ctx.env]}/api/v2/s3-upload/', ctx.session)
 
-    try:
-        result = create_accession(ctx.session, cohort_id, field_value)
-    except HTTPError as e:
-        if 'non_field_errors' in e.response.json():
-            click.secho(e.response.json()['non_field_errors'][0], fg='red', err=True)
-            sys.exit(1)
-        else:
-            raise
+        with accession.open('rb') as file_stream:
+            field_value = s3ff_client.upload_file(
+                file_stream,
+                accession.name,
+                'ingest.Accession.original_blob',
+            )
+
+        try:
+            result = create_accession(ctx.session, cohort_id, field_value)
+        except HTTPError as e:
+            if 'non_field_errors' in e.response.json():
+                click.secho(e.response.json()['non_field_errors'][0], fg='red', err=True)
+                sys.exit(1)
+            else:
+                raise
+
+    if json_:
+        click.echo(json.dumps(result, indent=2))
     else:
         click.secho(f'Accession uploaded, id={result["id"]}.', fg='green')
         click.secho(
